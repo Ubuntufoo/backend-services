1de010c5ef5411b024ea233c5f88c886dd13d846
timothymurphy <tmurph44@wgu.edu>
2026-05-18 21:24:29 -0400

    fix: seed default app settings row

---
+++ supabase/migrations/20260518120000_seed_default_app_settings.sql
@@ -0,0 +1,37 @@
+insert into public.app_settings (
+  id,
+  capture_mode,
+  gemini_daily_limit,
+  max_order_syncs_per_day,
+  handling_days,
+  r2_retention_days_after_sold,
+  incoming_folder_path,
+  processed_folder_path,
+  merchant_location_key,
+  office_location_name,
+  default_payment_policy_id,
+  default_fulfillment_policy_id,
+  default_return_policy_id,
+  default_shipping_profile,
+  default_package_type,
+  ebay_marketplace_id
+)
+values (
+  'default',
+  'single_1_image',
+  500,
+  25,
+  2,
+  30,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null
+)
+on conflict (id) do nothing;
