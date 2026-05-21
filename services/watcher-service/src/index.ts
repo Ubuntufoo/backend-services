5dc2617fe4b36c63376169726cabc2cd7da00e6b
timothymurphy <tmurph44@wgu.edu>
2026-05-21 15:42:57 -0400

    feat: processed watcher moves

--- services/watcher-service/src/index.ts
+++ services/watcher-service/src/index.ts
@@ -1,4 +1,6 @@
 export * from './config/index.js';
 export * from './data/index.js';
+export * from './file-move.js';
 export * from './image-grouping.js';
 export * from './listing-id.js';
+export * from './processed-paths.js';
